24118bb88fcbf99b896a45316abaa23dd67752f4
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2023-03-03 21:32:03 +0600

    static keyword

---
+++ static.js
@@ -0,0 +1,18 @@
+class Animal{
+    static category = 'Dog'
+    location = 'Rangpur'
+    constructor(name, speed ) {
+        this.name = name,
+        this.speed = speed
+    }
+    compare(animal1, animal2){
+        if(animal1.speed > animal2.speed){
+            return `${animal1.speed} is Faster`
+        }
+        return `${animal2.speed} is Faster`
+    }
+    
+}
+
+const myRat = new Animal("Rat", 12)
+console.log(Animal.category);
