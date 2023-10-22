4c50c6f48a91bf936dc2903130ed097116b5e548
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2023-10-23 01:14:42 +0600

    static properties and methods

---
+++ practice/6_static.js
@@ -0,0 +1,31 @@
+// Utility Function
+// let id = 1;
+// class User {
+//     static id = 1;
+//     constructor(name, age, income) {
+//         this.name = name;
+//         this.age = age;
+//         this.income = income;
+//         this.id = User.id++;
+//     }
+//     // static compareByAge (user1, user2) {
+//     //     return user1.age - user2.age;
+//     // }
+//     // static compareByIncome(user1, user2){
+//     //     return user1.income - user2.income;
+//     // }
+// }
+
+// const user1 = new User('Tushar', 40, 100)
+// const user2 = new User('Munir', 11, 500)
+// const user3 = new User('Gaus', 22, 300)
+
+// const users = [user1, user2, user3]
+// // users.sort(User.compareByIncome)
+// console.log(users)
+
+
+class Config {
+    static dbUser = 'username'
+    static dbPassword = 'serect'
+}
